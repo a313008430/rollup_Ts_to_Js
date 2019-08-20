0f33b9d0b0bb44f8eaea2f96eea6d3b40732de0e
Mr.x <313008430@qq.com>
2019-08-20 22:29:36 +0800

    更新修复如果有ts2的版本生成map的问题

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,27 @@
-import typescript from 'rollup-plugin-typescript';
-import server from 'rollup-plugin-serve';
+import typescript from 'rollup-plugin-typescript2';
+let defaults = { compilerOptions: { declaration: true } };
 
+/**
+ * 如果使用rollup-plugin-typescript2版本为2的这个库
+ * 想要更好的使用sourcemap除了这个文件里面要配置以外
+ * 在tsconfig.json里面需要配置sourcemap：true这边才会生效
+ */
 export default {
-    input: './app/main.ts',
+    input: 'main.ts',
     plugins: [
-        typescript({ lib: ["es5", "es6", "dom"], target: "es5", sourceMap: true }),
-        server({
-            host:'192.168.1.3',
-            port:888,
-            contentBase:'../'
-        })
+        // typescript({ lib: ["es5", "es6", "dom"], target: "es5", sourceMap: true }),
+        typescript({ tsconfig: 'tsconfig.json' }),
+        // server({
+        //     host:'192.168.1.3',
+        //     port:888,
+        //     contentBase:'../'
+        // })
     ],
     output: {
         file: 'bundle.js',
         // format: 'es'
-        sourceMap: true,
-        format: 'es'
+        format: 'umd',
+        sourcemap: 'inline',
+
     }
 }
